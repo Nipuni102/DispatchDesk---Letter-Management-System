91ca3ab934ede317e00fd671594dc46cb5a60dee
Chandupa2001 <chanduparanawaka2001@gmail.com>
2025-01-09 09:30:40 +0530

    Connected sections backend with frontend

--- src/Components/AccountsReg.js
+++ src/Components/AccountsReg.js
@@ -1,39 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/AccountsReg.css";
+import axios from "axios";
 
 const AccountsReg = () => {
-  const initialData = [
-    {
-      date: "2024-12-01",
-      refNo: "12345",
-      company: "ABC Corp",
-      subject: "Budget Allocation",
-      officerNo: "Officer 1",
-    },
-    {
-      date: "2024-12-02",
-      refNo: "67890",
-      company: "XYZ Pvt Ltd",
-      subject: "Contract Review",
-      officerNo: "Officer 2",
-    },
-  ];
-
   // State for managing search and filtered data
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState(initialData);
+  const [filteredData, setFilteredData] = useState([]);
 
   // Modal state
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const [actionTaken, setActionTaken] = useState("");
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchRecords();
+  }, [])
+  
+  // fetch data from database
+  const fetchRecords = async () => {
+    const url = "http://localhost:4000/api/accounts/getRegistered"
+
+    try {
+      const response = await axios.get(url);
+
+      if (response.data.success) {
+        setData(response.data.data)
+        setFilteredData(response.data.data)
+      } else {
+        console.log(response.data.message)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   // Handle search functionality
   const handleSearch = (e) => {
     const query = e.target.value.toLowerCase();
     setSearchQuery(query);
 
-    const filtered = initialData.filter(
+    const filtered = data.filter(
       (item) =>
         item.refNo.toLowerCase().includes(query) ||
         item.company.toLowerCase().includes(query) ||
